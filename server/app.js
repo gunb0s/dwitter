40a60785b966b8b70b6a9f0a6d183bea8395c198
gunb0s <bys20411@gmail.com>
2022-06-14 18:05:39 +0900

    init

---
+++ server/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+
+const PORT = 8080;
+
+const app = express();
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+
+app.listen(PORT, () => {
+  console.log(`Listening server ${PORT}...`);
+});
