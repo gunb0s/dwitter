1b03ca4f1b187209d1270ec9384b5a4d4f6d6ac6
gunb0s <bys20411@gmail.com>
2022-06-18 12:31:00 +0900

    Impelement tweetinput component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import TweetInput from "./components/TweetInput";
 import TwitterBar from "./components/TwitterBar";
 
 const wrapper =
   "w-screen h-screen flex justify-center items-center overflow-auto";
-const main = "w-[36rem] h-[50rem] bg-slate-200 drop-shadow-2xl";
+const main = "w-[36rem] h-[50rem] bg-slate-200 drop-shadow-2xl flex flex-col";
 
 const App = () => {
   return (
     <div className={wrapper}>
       <main className={main}>
         <TwitterBar />
+        <TweetInput />
       </main>
     </div>
   );
--- client/src/components/TweetInput.jsx
+++ client/src/components/TweetInput.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const TweetInput = () => {
-  return <div>TweetInput</div>;
+  return (
+    <div className="w-full">
+      <input
+        className="w-full outline-none p-2"
+        type="text"
+        placeholder="Enter tweet..."
+      />
+    </div>
+  );
 };
 
 export default TweetInput;
