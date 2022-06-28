e6f714b85ecac319a7cd3e063d02478022a47247
gunb0s <bys20411@gmail.com>
2022-06-28 12:40:51 +0900

    implement authcontext

--- client/src/index.js
+++ client/src/index.js
@@ -5,15 +5,20 @@ import "./index.css";
 import App from "./App";
 import { TweetService } from "./services/tweet";
 import { AxiosInstance } from "./network/http";
+import { AuthErrorEventBus, AuthProvider } from "./context/AuthContext";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const baseURL = process.env.REACT_APP_SERVER_URL;
+const authErrorEventBus = new AuthErrorEventBus();
 const axiosInstacne = new AxiosInstance();
 const tweetService = new TweetService(axiosInstacne);
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App tweetService={tweetService} />
+      <AuthProvider authErrorEventBus={authErrorEventBus}>
+        <App tweetService={tweetService} />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
